ceb3d017f9d7cb80f1c14890b2fd99c83695aa73
Chinmay Shrivastava <79005030+ChinmayShrivastava@users.noreply.github.com>
2023-12-04 20:03:59 -0500

    Update resumeobject.py

--- resumeobject.py
+++ resumeobject.py
@@ -3,8 +3,29 @@ import json
 from search import *
 from prompts import COMPARE_RESUME
 from openai import OpenAI
+from agents import *
+import time
+import streamlit as st
 
 client = OpenAI()
+
+AGENT_ID = st.secrets['AGENT_ID2']
+
+def informalize_resume_notes(notes):
+    message = {
+        "role": "user",
+        "content": notes
+    }
+    threadid = create_thread([message]).id
+    runid = run_assistant(AGENT_ID, threadid).id
+    while True:
+        time.sleep(2)
+        run = get_run(runid, threadid)
+        if run.status == 'completed':
+            break
+    response = get_response(threadid)
+    last_message = response.data[0].content[0].text.value
+    return last_message
 
 def generate_completion(prompt):
     # response = client.completions.create(
@@ -47,7 +68,7 @@ class Resume():
 
     def __init__(self, job_description, resume_experience):
         self.job_description = job_description
-        self.resume_experience = resume_experience
+        self.resume_experience = informalize_resume_notes(resume_experience)
         self.data = open_data_json('general0')
         self.embeddings = open_embeddings_json('general0_embeddings')
         self.fs_prompt = "Based on the job description and notes, explain step-by-step how to write the resume's experience section and then output the resume's experience section as illustrated in some examples before.\n"
@@ -56,6 +77,8 @@ class Resume():
         self.results = None
 
     def perform_similarity_search(self):
+        if self.job_description == '' or self.resume_experience == '':
+            return
         self.results = vector_search(self.job_description, self.embeddings, self.data)[0:3]
         i = 0
         for result in self.results:
