b05bc590e56fbae9e980ff908c1fe91a174bfc19
Chinmay Shrivastava <79005030+ChinmayShrivastava@users.noreply.github.com>
2023-12-05 12:18:08 -0500

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -6,11 +6,15 @@ st.title("Resume Updater App")
 # Create placeholders for the text areas
 job_description_placeholder = st.empty()
 resume_experience_placeholder = st.empty()
+disclaimer_placeholder = st.empty()
 
 # copy your text here for job description
-job_description = job_description_placeholder.text_area("Job Description", height=200)
+job_description = job_description_placeholder.text_area("Job Description", height=200, value='Enter the job description for the job and role you are applying for.', max_chars=750)
 # copy your text here for resume experience section notes
-resume_experience = resume_experience_placeholder.text_area("Old Resume Experience Section or Notes on your Experiences", height=200)
+resume_experience = resume_experience_placeholder.text_area("Old Resume Experience Section or Notes on your Experiences", height=200, value='Enter your resume\'s experience section or notes on your experiences.\nMake sure to not include anything else.', max_chars=750)
+disclaimer_placeholder.markdown("""Disclaimer note at the bottom: If you don't like the output try running a few times. Change up the text in the experience section. We've used it to better our own resumes! 
+
+P.S : Make sure to double check the output. It does hallucinate (i.e make up things) at times and is far from perfect.""")
 
 # if the user clicks the button generate resume object
 if st.button("Generate Resume"):
